268f738faa52bf843b685e54b6f8e60b66cbccbd
Kevin <kmai-amazon@users.noreply.github.com>
2021-06-16 17:59:28 -0500

    Clean up config

--- apps/chat/src/Config.js
+++ apps/chat/src/Config.js
@@ -2,12 +2,12 @@
 // SPDX-License-Identifier: MIT-0
 
 const appConfig = {
-  apiGatewayInvokeUrl:'https://qpik87xt5d.execute-api.us-east-1.amazonaws.com/Stage/',
-  cognitoUserPoolId: 'us-east-1_gh1exTMft',
-  cognitoAppClientId: '5gl5cgujescdvon81cchqkqrf7',
-  cognitoIdentityPoolId: 'us-east-1:209669ce-49b3-4566-a38b-f1d2cac65f17',
-  appInstanceArn: 'arn:aws:chime:us-east-1:946724017157:app-instance/49a93b15-d401-4d17-a111-f7c59d1b1298',
+  apiGatewayInvokeUrl:'',
+  cognitoUserPoolId: '',
+  cognitoAppClientId: '',
+  cognitoIdentityPoolId: '',
+  appInstanceArn: '',
   region: 'us-east-1',  // Only supported region for Amazon Chime SDK Messaging as of this writing
-  attachments_s3_bucket_name: 'chimesdkmessagingdemo-chatattachmentsbucket-davr97iqco15'
+  attachments_s3_bucket_name: ''
 };
 export default appConfig;
