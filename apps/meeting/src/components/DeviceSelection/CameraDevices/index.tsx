41d75484653adfa6b770d20d86db5a55fe49b356
Venkatesh Devale <devalevd@amazon.com>
2021-11-17 18:10:40 -0800

    Prepare dev branch

--- apps/meeting/src/components/DeviceSelection/CameraDevices/index.tsx
+++ apps/meeting/src/components/DeviceSelection/CameraDevices/index.tsx
@@ -7,7 +7,8 @@ import {
   PreviewVideo,
   QualitySelection,
   CameraSelection,
-  Label
+  Label,
+  BackgroundBlurCheckbox
 } from 'amazon-chime-sdk-component-library-react';
 
 import { title, StyledInputGroup } from '../Styled';
@@ -23,6 +24,9 @@ const CameraDevices = () => {
       </StyledInputGroup>
       <StyledInputGroup>
         <QualitySelection />
+      </StyledInputGroup>
+      <StyledInputGroup>
+        <BackgroundBlurCheckbox />
       </StyledInputGroup>
       <Label style={{ display: 'block', marginBottom: '.5rem' }}>
         Video preview
