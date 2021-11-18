752713b932829ac39b5f7582e4796a918d71d7c5
Sichao Xue <sichax@amazon.com>
2021-11-18 14:13:02 -0800

    Improve code style of api.ts file

--- apps/meeting/src/utils/api.ts
+++ apps/meeting/src/utils/api.ts
@@ -7,27 +7,31 @@ export const BASE_URL = routes.HOME;
 
 interface MeetingResponse {
   JoinInfo: {
-    Attendee: any;
-    Meeting: any;
+    Attendee: string;
+    Meeting: string;
   };
+}
+
+interface GetAttendeeResponse {
+  name: string;
 }
 
 export async function fetchMeeting(
   meetingId: string,
   name: string,
-  region: string
+  region: string,
 ): Promise<MeetingResponse> {
-  const response = await fetch(
-    `${BASE_URL}join?title=${encodeURIComponent(
-      meetingId
-    )}&name=${encodeURIComponent(name)}${
-      region ? `&region=${encodeURIComponent(region)}` : ''
-    }`,
-    {
-      method: 'POST'
-    }
-  );
-  const data = await response.json();
+  const params = {
+    title: encodeURIComponent(meetingId),
+    name: encodeURIComponent(name),
+    region: encodeURIComponent(region),
+  };
+
+  const res = await fetch(BASE_URL + 'join?' + new URLSearchParams(params), {
+    method: 'POST',
+  });
+
+  const data = await res.json();
 
   if (data.error) {
     throw new Error(`Server error: ${data.error}`);
@@ -36,36 +40,44 @@ export async function fetchMeeting(
   return data;
 }
 
-export function createGetAttendeeCallback(meetingId: string) {
-  return async (chimeAttendeeId: string, externalUserId?: string) => {
-    const attendeeUrl = `${BASE_URL}attendee?title=${encodeURIComponent(
-      meetingId
-    )}&attendee=${encodeURIComponent(chimeAttendeeId)}`;
-    const res = await fetch(attendeeUrl, {
-      method: 'GET'
-    });
+export async function getAttendee(
+  meetingId: string,
+  chimeAttendeeId: string
+): Promise<GetAttendeeResponse> {
+  const params = {
+    title: encodeURIComponent(meetingId),
+    attendee: encodeURIComponent(chimeAttendeeId),
+  };
 
-    if (!res.ok) {
-      throw new Error('Invalid server response');
-    }
+  const res = await fetch(BASE_URL + 'attendee?' + new URLSearchParams(params), {
+    method: 'GET',
+  });
 
-    const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Invalid server response');
+  }
 
-    return {
-      name: data.AttendeeInfo.Name
-    };
+  const data = await res.json();
+
+  return {
+    name: data.AttendeeInfo.Name,
   };
 }
 
-export async function endMeeting(meetingId: string) {
-  const res = await fetch(
-    `${BASE_URL}end?title=${encodeURIComponent(meetingId)}`,
-    {
-      method: 'POST'
-    }
-  );
+export async function endMeeting(meetingId: string): Promise<void> {
+  const params = {
+    title: encodeURIComponent(meetingId),
+  };
+
+  const res = await fetch(BASE_URL + 'end?' + new URLSearchParams(params), {
+    method: 'POST',
+  });
 
   if (!res.ok) {
     throw new Error('Server error ending meeting');
   }
 }
+
+export const createGetAttendeeCallback = (meetingId: string) =>
+  (chimeAttendeeId: string): Promise<GetAttendeeResponse> =>
+    getAttendee(meetingId, chimeAttendeeId);
