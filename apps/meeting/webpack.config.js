8be78be276fc735e228bd77f5f5832f53b6dc5c4
Sichao Xue <sichax@amazon.com>
2021-12-20 10:36:27 -0800

    Add /end API to dev server proxy

--- apps/meeting/webpack.config.js
+++ apps/meeting/webpack.config.js
@@ -58,7 +58,7 @@ module.exports = {
   ],
   devServer: {
     proxy: {
-      context: ['/join', '/attendee'],
+      context: ['/join', '/attendee', '/end'],
       target: 'http://127.0.0.1:8080',
     },
     historyApiFallback: {
