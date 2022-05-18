43ef1d0380a990d20e1563a04f8fa722c46775d5
Elias Demesie <99214179+eliasdem@users.noreply.github.com>
2022-05-18 15:30:46 -0500

    Add SAM support; Serve the demo app via S3 and CloudFront (#200)

--- apps/chat/src/Config.js
+++ apps/chat/src/Config.js
@@ -1,13 +1,16 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
+import data from './backend/serverless/appconfig.json'
+const appConfigJson = Object.assign({}, ...data.map((x) => ({[x.OutputKey]: x.OutputValue})));
+
 const appConfig = {
-  apiGatewayInvokeUrl:'',
-  cognitoUserPoolId: '',
-  cognitoAppClientId: '',
-  cognitoIdentityPoolId: '',
-  appInstanceArn: '',
-  region: 'us-east-1',  // Only supported region for Amazon Chime SDK Messaging as of this writing
-  attachments_s3_bucket_name: ''
+    apiGatewayInvokeUrl: '' || appConfigJson.apiGatewayInvokeUrl,
+    cognitoUserPoolId: '' || appConfigJson.cognitoUserPoolId,
+    cognitoAppClientId: '' || appConfigJson.cognitoAppClientId,
+    cognitoIdentityPoolId: '' || appConfigJson.cognitoIdentityPoolId,
+    appInstanceArn: '' || appConfigJson.appInstanceArn,
+    region: 'us-east-1',  // Only supported region for Amazon Chime SDK Messaging as of this writing
+    attachments_s3_bucket_name: '' || appConfigJson.attachments_s3_bucket_name
 };
 export default appConfig;
