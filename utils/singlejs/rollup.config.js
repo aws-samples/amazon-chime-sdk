a64735dbe66c95c8f18611de257fe419dc8037c3
Levi Sklaroff <lesklaro@amazon.com>
2022-10-25 14:07:19 -0700

    Fix singlejs not working with latest version

--- utils/singlejs/rollup.config.js
+++ utils/singlejs/rollup.config.js
@@ -1,21 +1,30 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
+import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import json from "@rollup/plugin-json";
+import packageJson from "./package.json";
+import progress from 'rollup-plugin-progress';
+import sizes from 'rollup-plugin-sizes';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   output: [
     {
-      file: 'build/amazon-chime-sdk.min.js',
-      format: 'umd',
+      file: 'build/amazon-chime-sdk.js',
+      format: 'iife',
       name: 'ChimeSDK',
       sourcemap: true,
     },
   ],
-  plugins: [resolve(), commonjs(), terser()],
+  plugins: [
+    nodeResolve({mainFields: ['module', 'main', 'browser'], preferBuiltins: true,}), typescript(),commonjs({include: [
+      'node_modules/'
+      ]}),json(),progress(), sizes({details: false})],
   onwarn: (warning, next) => {
     if (warning.code === 'CIRCULAR_DEPENDENCY') {
       // TODO: Fix https://github.com/aws/amazon-chime-sdk-js/issues/107
