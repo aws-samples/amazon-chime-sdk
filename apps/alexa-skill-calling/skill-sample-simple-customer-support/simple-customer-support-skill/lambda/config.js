a29ec529ca58a4c46298254e1662916314c18085
Gary Zhong <garyzhon@amazon.com>
2022-11-21 13:58:44 -0800

    Add Alexa skill calling sample skill
    Amazon Chime SDK Alexa skill calling allows enterprise customers to enable
    calling directly in their Amazon Alexa Skills. In this demo skill, we build
    an Alexa skill that allows a customer to place a call to a customer support.
    
    Tests
    Deploy this Alexa skill using Alexa Developer Console and deploy this
    SMA into Amazon Chime SDK PSTN Audio. Use this Alexa skill to call into
    the SMA.

---
+++ apps/alexa-skill-calling/skill-sample-simple-customer-support/simple-customer-support-skill/lambda/config.js
@@ -0,0 +1,10 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+const skillConfig = {
+  destinationPhoneNumber: 'placeholder',
+  sourcePhoneNumber: 'placeholder',
+};
+
+module.exports = {
+  skillConfig,
+};
