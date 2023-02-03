4c2641b8a78b6461e4194b9ca49d96a948837089
Levi Sklaroff <lesklaro@amazon.com>
2023-02-03 13:31:03 -0800

    Single JS rollup fix

--- utils/singlejs/rollup.config.js
+++ utils/singlejs/rollup.config.js
@@ -1,15 +1,11 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
-import typescript from 'rollup-plugin-typescript2';
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import json from "@rollup/plugin-json";
-import packageJson from "./package.json";
-import progress from 'rollup-plugin-progress';
-import sizes from 'rollup-plugin-sizes';
 
 export default {
   input: 'src/index.ts',
@@ -22,9 +18,14 @@ export default {
     },
   ],
   plugins: [
-    nodeResolve({mainFields: ['module', 'main', 'browser'], preferBuiltins: true,}), typescript(),commonjs({include: [
-      'node_modules/'
-      ]}),json(),progress(), sizes({details: false})],
+      resolve({
+        browser: true,
+        mainFields: ['module', 'browser'],
+      }),
+      json(),
+      commonjs(),
+      terser()
+    ],
   onwarn: (warning, next) => {
     if (warning.code === 'CIRCULAR_DEPENDENCY') {
       // TODO: Fix https://github.com/aws/amazon-chime-sdk-js/issues/107
