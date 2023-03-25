58d09bd44bc35591f2ce0bc9365a7e039b447f6f
Venkatesh Devale <devalevd@amazon.com>
2023-03-24 17:54:55 -0700

    Add meeting status notifier to React component lib meeting demo
    - This will help in testing various meeting statuses from Amazon Chime SDK
    component library for React.
    - Remove incorrect stale CONTRIBUTING guideline.
    - Update README with instructions on updating the `amazon-chime-sdk-component-library-react`
    submodule.

--- apps/meeting/CONTRIBUTING.md
+++ apps/meeting/CONTRIBUTING.md
@@ -21,44 +21,7 @@ reported the issue. Please try to include as much information as you can. Detail
 
 
 ## Contributing via Pull Requests
-Contributions via pull requests are much appreciated. Before sending us a pull request, please ensure that:
-
-1. Fork the repository and make sure you are working against the latest source on the *meeting-dev* branch. We use *meeting-dev* branch for `apps/meeting` app to contribute all PRs. This app is used to test the changes releasing into the next [amazon-chime-sdk-component-library-react](https://github.com/aws/amazon-chime-sdk-component-library-react) release.
-2. Run the meeting demo locally first. The *meeting-dev* apps/meeting demo changes are unstable and contributors will need to create and install the `amazon-chime-sdk-component-library-react` tarball from its *master* branch to start the demo.
-
-    1. In a different directory, create `amazon-chime-sdk-component-library-react` tarball.
-    ```
-    git clone https://github.com/aws/amazon-chime-sdk-component-library-react &&
-    cd amazon-chime-sdk-component-library-react &&
-    npm install &&
-    npm run build &&
-    npm pack
-    ```
-    This will create a tarball named: `amazon-chime-sdk-component-library-react-<latest-released-npm-version>.tgz`
-
-    2. Install the tarball in the meeting app
-    ```
-    cd <meeting-dev branch checked out from the aws-samples/amazon-chime-sdk cloned repository>
-    cd apps/meeting
-    npm install
-    npm install <path-to-amazon-chime-sdk-component-library-react-tarball>
-    ```
-
-    3. Start the app:
-    ```
-    npm run start
-    ```
-    
-    4. Open the app using https://0.0.0.0:9000/ in a browser.
-
-3. Modify source and ensure local demo app testing works as expected.
-4. Commit to your fork using clear commit messages.
-5. Send us a pull request, answering any default questions in the pull request interface.
-6. The PR should open against the *meeting-dev* branch and not the *main* branch.
-> Note that *meeting-dev* branch is for testing the next release, hence contributor must create PR against the meeting-dev branch.
-The *main* branch is a stable demo branch for the last release and any changes after a release must go into the *meeting-dev* branch.
-
-7. When opening PR, exclude any `package.json` changes for `the amazon-chime-sdk-component-library-react` dependency.
+Contributions via pull requests are much appreciated. Refer README for demo running instructions.
 
 GitHub provides additional document on [forking a repository](https://help.github.com/articles/fork-a-repo/) and
 [creating a pull request](https://help.github.com/articles/creating-a-pull-request/).
---
+++ apps/meeting/src/containers/MeetingStatusNotifier/index.tsx
@@ -0,0 +1,107 @@
+import {
+  ActionType,
+  MeetingStatus,
+  Severity,
+  useMeetingStatus,
+  useNotificationDispatch,
+} from 'amazon-chime-sdk-component-library-react';
+import React, { useEffect, useState } from 'react';
+import routes from '../../constants/routes';
+import { useHistory } from 'react-router-dom';
+
+type DemoMeetingStatus = 'connecting' | 'connected' | 'reconnecting' | 'disconnected' | 'failed';
+
+const MeetingStatusNotifier: React.FC = () => {
+  const meetingStatus = useMeetingStatus();
+  const dispatch = useNotificationDispatch();
+  const [status, setStatus] = useState<DemoMeetingStatus>();
+  const history = useHistory();
+
+  const getMeetingStatusPayload = (message: string, severity: Severity) => {
+    return {
+      severity,
+      message,
+      autoClose: true,
+      replaceAll: true,
+    };
+  };
+
+  useEffect(() => {
+    switch (meetingStatus) {
+      case MeetingStatus.Loading:
+        setStatus('connecting');
+        dispatch({
+          type: ActionType.ADD,
+          payload: getMeetingStatusPayload('Meeting connecting...', Severity.INFO),
+        });
+        break;
+      case MeetingStatus.Succeeded:
+        setStatus('connected');
+        if (status === 'reconnecting') {
+          dispatch({
+            type: ActionType.ADD,
+            payload: getMeetingStatusPayload('Meeting reconnected', Severity.SUCCESS),
+          });
+        } else {
+          dispatch({
+            type: ActionType.ADD,
+            payload: getMeetingStatusPayload('Meeting connected', Severity.SUCCESS),
+          });
+        }
+        break;
+      case MeetingStatus.Reconnecting:
+        setStatus('reconnecting');
+        dispatch({
+          type: ActionType.ADD,
+          payload: getMeetingStatusPayload('Meeting reconnecting...', Severity.WARNING),
+        });
+        break;
+      case MeetingStatus.Failed:
+        setStatus('failed');
+        dispatch({
+          type: ActionType.ADD,
+          payload: getMeetingStatusPayload(
+            'Meeting failed even after reconnection attempts, redirecting to home',
+            Severity.ERROR
+          ),
+        });
+        history.push(routes.HOME);
+        break;
+      case MeetingStatus.TerminalFailure:
+        setStatus('failed');
+        dispatch({
+          type: ActionType.ADD,
+          payload: getMeetingStatusPayload(
+            'Meeting will not reconnect due to fatal failure, redirecting to home',
+            Severity.ERROR
+          ),
+        });
+        history.push(routes.HOME);
+        break;
+      default:
+        break;
+    }
+    return () => {
+      setStatus(undefined);
+    };
+  }, [meetingStatus]);
+
+  useEffect(() => {
+    let id: number;
+    if (status === 'reconnecting') {
+      id = setInterval(() => {
+        dispatch({
+          type: ActionType.ADD,
+          payload: getMeetingStatusPayload('Meeting reconnecting...', Severity.WARNING),
+        });
+      }, 10 * 1000);
+    }
+    return () => {
+      clearInterval(id);
+    };
+  }, [status]);
+
+  return null;
+};
+
+export default MeetingStatusNotifier;
