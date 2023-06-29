ddf3ec7783cb72b2f4fb55d82e2101d3de4433aa
Michael Hyun <michhyun@amazon.com>
2023-06-29 14:49:39 -0700

    Update Amplify import

--- apps/amplify-demo/src/index.tsx
+++ apps/amplify-demo/src/index.tsx
@@ -3,24 +3,22 @@
 
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
-import {
-  MeetingProvider,
-  lightTheme
-} from 'amazon-chime-sdk-component-library-react';
+import { MeetingProvider, lightTheme } from 'amazon-chime-sdk-component-library-react';
 import Meeting from './components/Meeting';
 import MeetingForm from './components/MeetingForm';
 
-import Amplify from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
 
 window.addEventListener('load', () => {
   ReactDOM.render(
-  <ThemeProvider theme={lightTheme}>
-    <MeetingProvider>
-      <MeetingForm />
-      <Meeting/>
-    </MeetingProvider>
-  </ThemeProvider>
-  , document.getElementById('root'));
+    <ThemeProvider theme={lightTheme}>
+      <MeetingProvider>
+        <MeetingForm />
+        <Meeting />
+      </MeetingProvider>
+    </ThemeProvider>,
+    document.getElementById('root')
+  );
 });
